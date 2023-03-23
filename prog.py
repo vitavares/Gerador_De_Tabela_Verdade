14a03f783db16637e928aa5e308866b47be6295a
Vitória Tavares <vitoriadesousat@gmail.com>
2023-03-23 20:54:23 -0300

    Novas ideias

--- prog.py
+++ prog.py
@@ -1,6 +1,9 @@
 import ttg
 
+# Como adicionar novas proposições?
 print('Tabela de p, q e r:\n')
 print(ttg.Truths(['p', 'q', 'r\n']))
+
+# Quantas operações o usuário deseja fazer?
 operacao = input('Qual operação você deseja realizar? ')
 print(ttg.Truths(['p', 'q', 'r'], [operacao]))
