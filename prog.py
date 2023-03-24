0228373f48a6a6ebc4ab772734b904038b7a7dd0
Vitória Tavares <vitoriadesousat@gmail.com>
2023-03-24 20:37:50 -0300

    Ideias

--- prog.py
+++ prog.py
@@ -5,5 +5,6 @@ print('Tabela de p, q e r:\n')
 print(ttg.Truths(['p', 'q', 'r\n']))
 
 # Quantas operações o usuário deseja fazer?
+# Utilizar listas
 operacao = input('Qual operação você deseja realizar? ')
 print(ttg.Truths(['p', 'q', 'r'], [operacao]))
