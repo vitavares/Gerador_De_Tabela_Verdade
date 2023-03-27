678a8476e80c2b57e9d1ff9f80245abc494d46c7
Vitória Tavares <vitoriadesousat@gmail.com>
2023-03-27 20:15:55 -0300

    Atualizar o prog.py

--- prog.py
+++ prog.py
@@ -5,6 +5,8 @@ print('Tabela de p, q e r:\n')
 print(ttg.Truths(['p', 'q', 'r\n']))
 
 # Quantas operações o usuário deseja fazer?
+# quant_operacoes = input('Quantas operações deseja fazer?')
+
 # Utilizar listas
 operacao = input('Qual operação você deseja realizar? ')
 print(ttg.Truths(['p', 'q', 'r'], [operacao]))
