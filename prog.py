0264b0214abba721600d9497f6d157704b7b40ab
Vitória Tavares <vitoriadesousat@gmail.com>
2023-04-08 19:49:24 -0300

    Start input de operações

--- prog.py
+++ prog.py
@@ -1,12 +1,21 @@
 import ttg
 
+i = 0
+listaOperacoes = []
+continuar = True
 # Como adicionar novas proposições?
 print('Tabela de p, q e r:\n')
 print(ttg.Truths(['p', 'q', 'r\n']))
 
 # Quantas operações o usuário deseja fazer?
-# quant_operacoes = input('Quantas operações deseja fazer?')
 
-# Utilizar listas
-operacao = input('Qual operação você deseja realizar? ')
-print(ttg.Truths(['p', 'q', 'r'], [operacao]))
+while continuar:
+    op = input("Digite uma para operação (ou 'fim' para ver o resultado da operações): ")
+    if op == "fim":
+        continuar = False
+    else:
+        listaOperacoes.append(op)
+
+print("As operações escolhidas foram:", listaOperacoes)
+
+# print(ttg.Truths(['p', 'q', 'r'], [operacao]))
